5e6f7422d7624ed6a8c097aa8250045f3df0ce68
Masson <l.masson@rns.ecolescreatives.com>
2020-11-18 17:17:21 +0100

    Update TicTacToeLeo.py

--- TicTacToeLeo.py
+++ TicTacToeLeo.py
@@ -1,12 +1,23 @@
 def initialiseGrille(grille[9]):
     compteur = 0
-    for compteur in range(0,8):
+    for compteur in range(0,9):
         grille[compteur] = "_"
     return()
 
 def afficheGrille(grille[9]):
     i = 0
-    for i in range(0,2):
+    for i in range(0,3):
         print(grille[3*i], grille[3*i+1], grille[3*i+2], "\n")
     return()
     
+def ajouteSymbole(grille[9], joueur):
+    i = 0
+    j = 0
+    choixIncorrect = True
+    while (choixIncorrect = True):
+        i = int(input("Sur quelle ligne voulez vous jouer ?"))
+        j = int(input("Sur quelle colonne voulez vous jouer ?"))
+        if(grille[3*i+j]!="_")
+            grille[3*i+j]!="_" = joueur
+            choixIncorrect = False
+    return()
